8f55e8c1002ca5402c623cafd4654d3e908a2548
eamre <emrealkan.1@outlook.com>
2021-12-25 01:43:40 +0300

    OdevDosyalarıEklendi
    Hafta2Odev

---
+++ Northwind.Dal/Abstract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Abstract
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        //--------repository pattern
+        bool BeginTransaction();
+        bool RollBackTransaction();
+        int SaveChanges();
+    }
+}
